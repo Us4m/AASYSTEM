4161399e17e9c88afd8090adaba8dbeaacd00d08
Us4m <usamathegreat7@icloud.com>
2022-12-03 18:24:39 +0500

    new chag 3dec

--- AASYSTEM/settings.py
+++ AASYSTEM/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 import os
 from pickle import TRUE
-import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -96,7 +96,7 @@ DATABASES = {
     }  
 }  
 
-DATABASES['default'] = dj_database_url.config()
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
